bd97d32f0604e7dc43f0179e8cc9732e1d00727e
Youness Bourgui <younessbourgui07@gmail.com>
2024-07-26 11:11:29 +0200

    Create The Product Controller

--- src/main/java/main/controller/ProductController.java
+++ src/main/java/main/controller/ProductController.java
@@ -4,15 +4,22 @@
  */
 package main.controller;
 
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import main.dto.ProductDTO;
+import main.exception.EntityNotFoundException;
 import main.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +45,70 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
         return ResponseEntity.ok(result);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Integer id){
+        var product = service.findById(id);
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    
+    @PostMapping
+    public ResponseEntity<ProductDTO> create(@Valid ProductDTO x){
+        var createdProduct = service.create(x);
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+        }catch(ConstraintViolationException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Integer id, @Valid ProductDTO x){
+        var updatedProduct = service.update(id, x);
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+        }catch(IllegalArgumentException | ConstraintViolationException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        try{
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    
+    @GetMapping("/search")
+    public ResponseEntity<Page<ProductDTO>> search(
+            @RequestParam(required=false) String name,
+            @RequestParam(required=false) String desc,
+            @RequestParam(required=false) Boolean discountStatus,
+            @RequestParam(required=false) String categoryName,
+            @RequestParam(required=false) Double minPrice,
+            @RequestParam(required=false) Double maxPrice,
+            @RequestParam(defaultValue="0") int page,
+            @RequestParam(defaultValue="10") int size
+    ){
+        var result = service.search(name, desc, discountStatus, categoryName, minPrice, maxPrice, page, size);
+        if(result.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
     
     
